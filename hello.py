a9ab26cfcca1ca871342675557c3f0e10493bfa5
Quan Nguyen <quangineer@gmail.com>
2019-06-24 10:14:31 -0700

    Change relative path to absolute path for css, js and img path

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import send_from_directory
+from random import randint
 app = Flask(__name__, static_url_path='/static')
 
 # url_for('static', filename='style.css')
@@ -9,19 +10,21 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template('index.html')
 
-
+# Other static resources
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('static/js', path)
-
 @app.route('/vendor/<path:path>')
 def send_vendor(path):
     return send_from_directory('static/vendor', path)
-
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('static/css', path)
-
 @app.route('/img/<path:path>')
 def send_img(path):
     return send_from_directory('static/img', path)
+
+# Blog post resources
+@app.route("/post/<postname>/")
+def post1(postname):
+    return render_template(postname)
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    console.log("Reach here");
-    alert("Measage");
-});
-alert("Measage");
+// $(document).ready(function() {
+//     console.log("Reach here");
+//     alert("Measage");
+// });
+// alert("Measage");
