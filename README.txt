88b8b23e872300a3bc94396c3b00f44f69eba888
Quan Nguyen <quangineer@gmail.com>
2019-06-25 13:51:23 -0700

    web

--- README.txt
+++ README.txt
@@ -1,6 +1,7 @@
 
 Run app:
 ```
+export TEMPLATES_AUTO_RELOAD = True
 export FLASK_APP=hello.py
 python3 -m flask run
 ```
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,26 @@
-// $(document).ready(function() {
-//     console.log("Reach here");
-//     alert("Measage");
-// });
-// alert("Measage");
+$(document).ready(function() {
+    console.log("Reach here");
+
+    $.ajax({
+        url: "https://dog.ceo/api/breeds/image/random",
+        type: "GET",
+        // This is the important part
+        xhrFields: {
+            withCredentials: false
+        },
+        // data: "",
+        dataType: 'json',
+        success: function (response) {
+            console.log(response.message)
+            console.log($(".intro-header")[0].style.background = "url(" + response. message + ")")
+            // $(":header").attr('src',response.message);
+
+            // $(":header").css("style", "background-image: url(" + response. message + ")");
+
+        },
+        error: function (xhr, status) {
+            console.log(xhr)
+            console.log(status)
+        }
+    });    
+});
